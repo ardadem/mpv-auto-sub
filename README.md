e36d14e3734dac75b67d0faea89a3d7a24933831
Arda <demir.arda.2000@gmail.com>
2019-03-23 19:36:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# mpv-auto-sub
+
+## Installation
+
+First, you need to install [subliminal](https://github.com/Diaoul/subliminal)
+
+Then just run this command
+
+```
+mkdir -p ~/.config/mpv/scripts; cd ~/.config/mpv/scripts; curl -O https://raw.githubusercontent.com/ardadem/mpv-auto-sub/master/auto-sub.lua
+```
+
+That's it!
+
+## Configuration
+
+Edit configuration section in `~/.config/mpv/scripts` with your favorite text editor.
+
+## Usage
+
+Play a video in mpv then press the 'B' key. Subtitle will be loaded.
---
+++ auto-sub.lua
@@ -0,0 +1,58 @@
+-- ** Configuration **
+local subliminalPath = "/usr/bin/subliminal"
+local subDownloadDirPath = "/tmp/mpv-auto-sub/"
+local subLanguage = "tr"
+local autoLoadDownloadedSub = true
+-- ** Configuration **
+
+local utils = require 'mp.utils'
+
+local vidName
+local vidPath
+local subPath
+
+function prepare()
+    vidName = mp.get_property('filename')
+    vidPath = mp.get_property('path')
+    subPath = subDownloadDirPath .. string.gsub(vidName, "%.%w+$", ".srt")
+    
+    if not exists(subDownloadDirPath) == true then
+        os.execute("mkdir " .. subDownloadDirPath)
+    else
+        if (autoLoadDownloadedSub == true and exists(subPath) == true) then
+            load_subtitle(subPath)
+	end
+    end
+end
+
+function run()
+    prepare()
+    mp.osd_message("Fetching subtitle")
+    local p = utils.subprocess({ args = {subliminalPath, "download", "-s", "-f", "-l", subLanguage, "-d", subDownloadDirPath, vidPath}})
+    if p.error == nil then
+        load_subtitle(subPath)
+    else
+        mp.osd_message("Subtitle couldn't fetch")
+    end
+end
+
+function load_subtitle(path)
+    if mp.commandv("sub_add", path) then
+        mp.osd_message("Subtitle successfully loaded")
+    else
+        mp.osd_message("Subtitle couldn't load")
+    end
+end
+
+function exists(file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            return true
+        end
+    end
+    return ok, err
+end
+
+mp.register_event("file-loaded", prepare)
+mp.add_key_binding('b', 'run_auto_sub', run)
